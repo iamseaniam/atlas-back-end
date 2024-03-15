e6370b8ff9d9b3c546722b18617df9212c7996cc
iamseaniam <7222@holbertonstudents.com>
2024-03-15 14:20:21 -0500

    PEP8 validation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,19 +14,22 @@ if __name__ == "__main__":
     URL = 'https://jsonplaceholder.typicode.com/'
     user_endpoint = f'{URL}users/{employee_id}'
     todos_endpoint = f'{URL}todos'
-    
+
     user_response = requests.get(user_endpoint)
     user_data = user_response.json()
-    
+
     todos_response = requests.get(todos_endpoint)
     todos_data = todos_response.json()
-    
-    employee_todos = [todo for todo in todos_data if todo['userId'] == int(employee_id)]
-    
+
+    employee_todos = [todo for todo in todos_data 
+                      if todo['userId'] == int(employee_id)]
+
     done_tasks = sum(1 for todo in employee_todos if todo['completed'])
     total_tasks = len(employee_todos)
-
-    print(f"Employee {user_data['name']} is done with tasks({done_tasks}/{total_tasks}):")
+    
+    print(f"Employee {user_data['name']} 
+          is done with tasks
+          ({done_tasks}/{total_tasks}):")
     for todo in employee_todos:
         if todo['completed']:
-            print(f"\t {todo['title']}")    
+            print(f"\t {todo['title']}")
